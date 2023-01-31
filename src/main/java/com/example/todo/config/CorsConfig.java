877ddb98ffca70ff41c6a09ac931d1cc792a6437
KimHaechan <ckssmsla98@naver.com>
2023-01-31 15:50:43 +0900

    war 배포 설정

---
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.example.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        // api cors 정책 설정 , @crossorigin X
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+
+    }
+}
